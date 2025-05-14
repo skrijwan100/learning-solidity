75ca9a4de452c8d44058e239e5cbf07dcc0d0d03
skrijwan100 <rijwansk329@gmail.com>
2025-05-14 11:46:26 +0530

    learn funtion

--- datatype.sol
+++ datatype.sol
@@ -4,4 +4,8 @@ pragma solidity 0.8.30;
 contract variable{
     int public number=123;
     uint public unumber=334;
+    string public name="skrijwan";
+    bool public isactive = true;
+    address public  metaaddress=0xe95a1FF5E1c28225FaC694Be2B87B3BFbE88117d;
+    bytes2 public ctrygore= "go";
 }
---
+++ function.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract lernfunction{
+    int public num=20;
+    int public num1=20;
+    int public num2=20;
+    function add() public view returns(int){
+        return num+10;
+    }
+}
