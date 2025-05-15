3e8d4bada0f90385e024c76585280cc3ecc3cdd6
skrijwan100 <rijwansk329@gmail.com>
2025-05-16 00:16:40 +0530

    new

---
+++ memory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract memoryconcept{
+    uint public num=10;
+
+    function paramiter(string memory name) public  pure returns (string memory){
+            return name;
+    }
+
+}
