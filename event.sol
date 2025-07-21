6b48c5c4a0b75314329b5b5a6171b8bb6e4e1131
skrijwan100 <rijwansk329@gmail.com>
2025-07-21 22:50:35 +0530

    update

--- event.sol
+++ event.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.30;
 contract test{
     event transfar(address _from, address _to, uint _amount);
 
-    function trans(address to, uint amout) public {
+    function trans(address to, uint  amout) public {
        emit transfar(msg.sender, to, amout);
 
     }
