9530760c2bafcc7068003d9fadd049748e628d3e
skrijwan100 <rijwansk329@gmail.com>
2025-07-24 09:50:23 +0530

    add new

--- datatype.sol
+++ datatype.sol
@@ -9,3 +9,11 @@ contract variable{
     address public  metaaddress=0xe95a1FF5E1c28225FaC694Be2B87B3BFbE88117d;
     bytes2 public ctrygore= "go";
 }
+
+contract helloWorld{
+    string public a="Hello World!";
+    function Show() public view returns(string memory) {
+        return a;
+    }
+
+}
